8c5e731ac3b6e557051d187c9db3cbd5c2138101
Siddhanta Bhatta <32597420+sidtYson@users.noreply.github.com>
2018-02-20 08:58:43 +0000

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,12 @@
 from flask import Flask
+#from mymodule import MyModule
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
+    '''mm = MyModule()
+    c = mm.printIt()
+    return c'''
     return "Hello World!"
 
 if __name__ == "__main__":
